1f9e3ed1d86a38d48a41a13bc7600a1caa829b5c
ShuMatsuki <shu.matsuki.0625@gmail.com>
2020-06-29 22:39:12 +0900

    プロジェクトと.gitignoreを追加

---
+++ SampleMVP/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SampleMVP
+//
+//  Created by 松木周 on 2020/06/29.
+//  Copyright © 2020 ShuMatsuki. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
---
+++ SampleMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleMVP
+//
+//  Created by 松木周 on 2020/06/29.
+//  Copyright © 2020 ShuMatsuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
